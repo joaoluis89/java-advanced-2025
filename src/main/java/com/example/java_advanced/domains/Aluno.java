59ea89414d10787de99d99e7b9cb7d0238d44057
Joao Pacheco <joao.luis.pacheco@alumni.usp.br>
2025-08-21 22:55:25 -0300

    Adds Spring JPA and H2 database

--- src/main/java/com/example/java_advanced/domains/Aluno.java
+++ src/main/java/com/example/java_advanced/domains/Aluno.java
@@ -1,13 +1,20 @@
 package com.example.java_advanced.domains;
 
+import jakarta.persistence.*;
 import lombok.*;
 
-@Builder
-@Getter
 @With
+@Getter
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Aluno {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
+    @Column(name = "name")
     private String nome;
     private String sobrenome;
     private String matricula;
--- src/main/java/com/example/java_advanced/services/CreateAlunoService.java
+++ src/main/java/com/example/java_advanced/services/CreateAlunoService.java
@@ -1,16 +1,25 @@
 package com.example.java_advanced.services;
 
 import com.example.java_advanced.domains.Aluno;
+import com.example.java_advanced.gateways.AlunoRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
+@RequiredArgsConstructor
 public class CreateAlunoService implements AlunoDataServiceInterface {
 
+    private final AlunoRepository alunoRepository;
+
     public Aluno execute(Aluno aluno) {
-        return aluno.
-                withId(UUID.randomUUID().toString());
+
+        Aluno alunoSaved = alunoRepository.save(aluno);
+        List<Aluno> alunosByIdade = alunoRepository.findByIdade(36);
+
+        return alunoSaved;
     }
 
 }
