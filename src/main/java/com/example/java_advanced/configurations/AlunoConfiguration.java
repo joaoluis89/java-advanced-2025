82a64dfaccaa218fbfe1214377b6652986c57caa
Joao Pacheco <joao.luis.pacheco@alumni.usp.br>
2025-09-01 21:40:34 -0300

    reinforcement exercise

--- src/main/java/com/example/java_advanced/configurations/AlunoConfiguration.java
+++ src/main/java/com/example/java_advanced/configurations/AlunoConfiguration.java
@@ -1,6 +1,7 @@
 package com.example.java_advanced.configurations;
 
 import com.example.java_advanced.services.ConcatIdToAlunoService;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +10,7 @@ public class AlunoConfiguration {
 
 
     @Bean
+    @ConditionalOnProperty(prefix = "service.aluno", name = "concat-id-to-aluno", havingValue = "!enabled")
     public ConcatIdToAlunoService concatIdToAlunoService() {
         return new ConcatIdToAlunoService();
     }
